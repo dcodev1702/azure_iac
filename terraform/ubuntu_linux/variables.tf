46444638ec4a850693f2417a82f783fda1ac740d
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-04-10 19:07:28 -0400

    Create variables.tf

---
+++ terraform/ubuntu_linux/variables.tf
@@ -0,0 +1,19 @@
+variable "end_user" {
+    type = string
+    default = "dcodev-1702"
+}
+
+variable "host_os" {
+    type = string
+    default = "windows"
+}
+
+variable "tag_env" {
+    type = string
+    default = "dev"
+}
+
+variable "rule_ip_addresses" {
+    type = string
+    default = "xx.xx.xx.xx/32"
+}
