8fd70f2a299bec4691fa503cf76e7679295f4685
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-04-19 20:34:48 -0400

    Update variables.tf

--- terraform/ubuntu_linux/variables.tf
+++ terraform/ubuntu_linux/variables.tf
@@ -3,18 +3,7 @@ variable "end_user" {
   default = "dcodev-1702"
 }
 
-# Replaced by local.check_os
-#variable "host_os" {
-#  type    = string
-#  default = "windows"
-#}
-
 variable "tag_env" {
   type    = string
   default = "dev"
 }
-
-#variable "rule_ip_addresses" {
-#  type    = string
-#  default = "xx.xx.xx.xx/32"
-#}
