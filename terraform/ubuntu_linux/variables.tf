d4d4f65785fe6308d0ecef27e15b230f0601cd96
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-05-30 00:54:22 -0400

    Update variables.tf

--- terraform/ubuntu_linux/variables.tf
+++ terraform/ubuntu_linux/variables.tf
@@ -3,6 +3,11 @@ variable "end_user" {
   default = "dcodev"
 }
 
+variable "ssh_key_name" {
+  type    = string
+  default = "secOpsAzureKey"
+}
+
 variable "tag_env" {
   type    = string
   default = "dev"
