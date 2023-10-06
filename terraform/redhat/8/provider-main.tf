1da26e8c0af261c009dcebef4f41eed34c085ece
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-10-06 03:57:24 -0400

    Update provider-main.tf

--- terraform/redhat/8/provider-main.tf
+++ terraform/redhat/8/provider-main.tf
@@ -3,15 +3,44 @@ terraform {
   required_version = "~> 1.5.7"
   required_providers {
     azurerm = {
-      version = "=3.75.0"
       source  = "hashicorp/azurerm"
+      version = "~> 3.75.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.3.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.1"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.3.1"
     }
   }
 }
 
 # Configure the Azure provider
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
   environment     = "public"
   subscription_id = var.azure_subscription_id
   client_id       = var.azure_client_id
