857e138929c64e70f32694f4f612b62a67720302
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-10-06 20:46:42 -0400

    Create provider-main.tf

---
+++ terraform/redhat/8/backend/provider-main.tf
@@ -0,0 +1,24 @@
+# Define Terraform provider
+terraform {
+  required_version = "~> 1.5.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.75.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+  }
+}
+
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
+  environment     = "public"
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+}
