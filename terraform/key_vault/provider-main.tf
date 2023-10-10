ac58f504907e48a331069af888d46a8a4a2fe2c2
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-10-10 18:57:47 -0400

    Update provider-main.tf

--- terraform/key_vault/provider-main.tf
+++ terraform/key_vault/provider-main.tf
@@ -19,7 +19,7 @@ terraform {
       version = "~> 3.4.0"
     }
   }
-  backend "azurerm" {
+  backend azurerm {
     resource_group_name  = "rg-terraform-devops"
     storage_account_name = "satfdevops07695"
     container_name       = "tfstate"
@@ -28,7 +28,7 @@ terraform {
 }
 
 # Configure the Azure provider
-provider "azurerm" {
+provider azurerm {
   features {
     key_vault {
       purge_soft_delete_on_destroy = true
