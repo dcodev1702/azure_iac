78936f22adfc1424d1314954993d49974e041081
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-10-10 16:53:54 -0400

    Create provider.tf

---
+++ terraform/ubuntu_linux/provider.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_version = "~> 1.6.1"
+  required_providers {
+     azurerm  = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.75.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.3.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.1"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.3.1"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-devops"
+    storage_account_name = "satfdevops07695"
+    container_name       = "tfstate"
+    key                  = "secops-00-vm-syslog.tfstate"
+  }
+}
+
+# Configure the Azure provider
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+  environment     = "public" # Cloud Types: Public, USGovernment, etc
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+}
