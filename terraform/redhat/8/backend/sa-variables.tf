344c00045fbb3f996cf669568f3190b272edca93
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-10-10 19:01:30 -0400

    Update sa-variables.tf

--- terraform/redhat/8/backend/sa-variables.tf
+++ terraform/redhat/8/backend/sa-variables.tf
@@ -1,28 +1,28 @@
-variable "location" {
+variable location {
   type        = string
   description = "Azure Commercial Region"
 }
-variable "resource_group_name" {
+variable resource_group_name {
   type        = string
   description = "Azure Resource Group Name"
 }
-variable "storage_account_name" {
+variable storage_account_name {
   type        = string
   description = "Azure Storage Account Name"
 }
-variable "storage_account_rg" {
+variable storage_account_rg {
   type        = string
   description = "Azure Storage Account Resource Group"
 }
-variable "storage_account_sku" {
+variable storage_account_sku {
   type        = string
   description = "Azure Storage Account SKU"
 }
-variable "sa_container_name" {
+variable sa_container_name {
   type        = string
   description = "Azure Storage Account Container Name"
 }
-variable "tag_env" {
+variable tag_env {
   type    = string
   default = "terraform backend storage"
 }
