bdfd4f668402687c18ca9698d439cfbe5a549c4d
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-10-10 18:59:28 -0400

    Update variables.tf

--- terraform/key_vault/variables.tf
+++ terraform/key_vault/variables.tf
@@ -1,12 +1,12 @@
-variable "tag_env" {
+variable tag_env {
   type    = string
   description = "Environment Tag"
 }
-variable "key_vault_name" {
+variable key_vault_name {
   type        = string
   description = "Name of Key Vault"
 }
-variable "key_vault_location" {
+variable key_vault_location {
   type        = string
   description = "Location of Key Vault"
 }
