1d9dedb12d04edce23d1faf188faa26d40afeaa8
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-10-10 04:16:57 -0400

    Update main.tf

--- terraform/ubuntu_linux/main.tf
+++ terraform/ubuntu_linux/main.tf
@@ -1,53 +1,141 @@
 terraform {
 
   required_providers {
-    azurerm = {
+     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.75.0"
+      version = "~> 3.75.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.3.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.1"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.3.1"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-devops"
+    storage_account_name = "satfdevops07695"
+    container_name       = "tfstate"
+    key                  = "secops-00-vm-syslog.tfstate"
+  }
 }
 
+# Configure the Azure provider
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+  environment     = "public" # Cloud Types: Public, USGovernment, etc
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
 }
 
-resource "azurerm_resource_group" "secOps" {
-  name     = "secOps-tf-rg"
-  location = "eastus"
+
+# Generate a random vm name
+resource "random_string" "rstring" {
+  length  = 8
+  upper   = false
+  numeric = true
+  lower   = true
+  special = false
+}
+
+resource "azurerm_resource_group" "secops" {
+  depends_on = [random_string.rstring]
+  name       = "secops-vm-tf-rg-${random_string.rstring.result}"
+  location   = var.location
   tags = {
     environment = var.tag_env
   }
 }
 
-resource "azurerm_virtual_network" "secOps-vnet" {
-  name                = "secOps-tf-vnet"
-  resource_group_name = azurerm_resource_group.secOps.name
-  location            = azurerm_resource_group.secOps.location
-  address_space       = ["10.123.0.0/16"]
+resource "random_uuid" "get-uuid" {}
+
+resource "random_id" "random_id" {
+  keepers = {
+    resource_group = azurerm_resource_group.secops.name
+  }
+  byte_length = 8
+}
+
+data "azurerm_key_vault" "main" {
+  name                = var.key_vault_name
+  resource_group_name = var.key_vault_resource_group_name
+}
+
+resource tls_private_key main {
+  algorithm  = "RSA"
+  rsa_bits   = 4096
+}
+
+# Create a secret (ssh public key) in the key vault
+resource azurerm_key_vault_secret ssh_public_key {
+  depends_on   = [tls_private_key.main, data.azurerm_key_vault.main]
+  key_vault_id = data.azurerm_key_vault.main.id
+  name         = "${local.hostname}-ssh-public-key"
+  value        = tls_private_key.main.public_key_openssh
+}
+
+# Create a secret (ssh private key) in the key vault
+resource azurerm_key_vault_secret ssh_private_key {
+  depends_on   = [tls_private_key.main, data.azurerm_key_vault.main]
+  key_vault_id = data.azurerm_key_vault.main.id
+  name         = "${local.hostname}-ssh-private-key"
+  value        = tls_private_key.main.private_key_pem
+}
+
+
+resource "azurerm_virtual_network" "secops-vnet" {
+  name                = "secops-tf-vnet-${random_id.random_id.hex}"
+  resource_group_name = azurerm_resource_group.secops.name
+  location            = azurerm_resource_group.secops.location
+  address_space       = [var.network_vnet_cidr]
   tags = {
     environment = var.tag_env
   }
 }
 
-resource "azurerm_subnet" "secOps-subnet" {
-  name                 = "secOps-tf-subnet"
-  resource_group_name  = azurerm_resource_group.secOps.name
-  virtual_network_name = azurerm_virtual_network.secOps-vnet.name
-  address_prefixes     = ["10.123.1.0/24"]
+resource "azurerm_subnet" "secops-subnet" {
+  name                 = "secops-tf-subnet-${random_id.random_id.hex}"
+  resource_group_name  = azurerm_resource_group.secops.name
+  virtual_network_name = azurerm_virtual_network.secops-vnet.name
+  address_prefixes     = [var.vm_subnet_cidr]
 }
 
-resource "azurerm_network_security_group" "secOps-nsg" {
-  name                = "secOps-tf-nsg"
-  location            = azurerm_resource_group.secOps.location
-  resource_group_name = azurerm_resource_group.secOps.name
+resource "azurerm_network_security_group" "secops-nsg" {
+  name                = "secops-tf-nsg-${random_id.random_id.hex}"
+  location            = azurerm_resource_group.secops.location
+  resource_group_name = azurerm_resource_group.secops.name
   tags = {
     environment = var.tag_env
   }
 }
 
-resource "azurerm_network_security_rule" "secOps-dev-ssh-rule" {
-  name                        = "secOps-tf-dev-ssh-rule"
+resource "azurerm_network_security_rule" "secops-dev-ssh-rule" {
+  name                        = "secops-tf-dev-ssh-rule"
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
@@ -56,80 +144,83 @@ resource "azurerm_network_security_rule" "secOps-dev-ssh-rule" {
   destination_port_range      = "22"
   source_address_prefix       = "${chomp(data.http.my-home-ip.response_body)}/32"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.secOps.name
-  network_security_group_name = azurerm_network_security_group.secOps-nsg.name
+  resource_group_name         = azurerm_resource_group.secops.name
+  network_security_group_name = azurerm_network_security_group.secops-nsg.name
 }
 
-resource "azurerm_subnet_network_security_group_association" "secOps-subnet-nsg" {
-  subnet_id                 = azurerm_subnet.secOps-subnet.id
-  network_security_group_id = azurerm_network_security_group.secOps-nsg.id
+resource "azurerm_subnet_network_security_group_association" "secops-subnet-nsg" {
+  subnet_id                 = azurerm_subnet.secops-subnet.id
+  network_security_group_id = azurerm_network_security_group.secops-nsg.id
 }
 
-resource "random_uuid" "get-uuid" {}
-
-resource "azurerm_public_ip" "secOps-ip" {
-  name                = "secOps-tf-ip-${random_uuid.get-uuid.result}"
-  location            = azurerm_resource_group.secOps.location
-  resource_group_name = azurerm_resource_group.secOps.name
+resource "azurerm_public_ip" "secops_ip" {
+  name                = "secops-tf-ip-${random_id.random_id.hex}"
+  location            = azurerm_resource_group.secops.location
+  resource_group_name = azurerm_resource_group.secops.name
   allocation_method   = "Dynamic"
   tags = {
     environment = var.tag_env
   }
 }
 
-resource "azurerm_network_interface" "secOps-nic" {
-  name                = "secOps-tf-nic"
-  location            = azurerm_resource_group.secOps.location
-  resource_group_name = azurerm_resource_group.secOps.name
+resource "azurerm_network_interface" "secops-nic" {
+  name                = "secops-tf-nic-${random_id.random_id.hex}"
+  location            = azurerm_resource_group.secops.location
+  resource_group_name = azurerm_resource_group.secops.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.secOps-subnet.id
+    subnet_id                     = azurerm_subnet.secops-subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.secOps-ip.id
+    public_ip_address_id          = azurerm_public_ip.secops_ip.id
   }
   tags = {
     environment = var.tag_env
   }
 }
 
-resource "azurerm_linux_virtual_machine" "secOps-linux-vm-01" {
-  name                  = "secOps-tf-linux-vm-01"
-  resource_group_name   = azurerm_resource_group.secOps.name
-  location              = azurerm_resource_group.secOps.location
+resource "azurerm_linux_virtual_machine" "secops-linux-vm" {
+  depends_on            = [
+    azurerm_network_interface.secops-nic,
+    azurerm_key_vault_secret.ssh_public_key,
+    azurerm_key_vault_secret.ssh_private_key
+  ]
+  name                  = local.hostname
+  resource_group_name   = azurerm_resource_group.secops.name
+  location              = azurerm_resource_group.secops.location
   size                  = "Standard_D2as_v4"
   admin_username        = var.vm_username
-  network_interface_ids = [azurerm_network_interface.secOps-nic.id]
+  network_interface_ids = [azurerm_network_interface.secops-nic.id]
 
   custom_data = base64encode(templatefile("${path.module}/init_script.tpl", { VM_USERNAME = "${var.vm_username}" }))
 
   admin_ssh_key {
     username   = var.vm_username
-    public_key = file(pathexpand("~/.ssh/${var.ssh_key_name}.pub"))
+    public_key = azurerm_key_vault_secret.ssh_public_key.value
   }
 
   os_disk {
-    name                 = "secOps-tf-linux-vm-01-osdisk"
+    name                 = "secops-vm-tf-osdisk-${random_id.random_id.hex}"
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
-    disk_size_gb         = "60"
+    disk_size_gb         = "80"
   }
 
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts-gen2"
-    version   = "22.04.202307250"
+    version   = "latest"
   }
 
   provisioner "local-exec" {
-      command = templatefile("${local.host_os}_ssh_vscode.tpl", {
+    command = templatefile("${local.host_os}_ssh_vscode.tpl", {
       hostname     = self.public_ip_address
       user         = var.vm_username
       username     = data.external.host_username.result.username
-      identityfile = pathexpand("~/.ssh/${var.ssh_key_name}")
+      identityfile = pathexpand("${path.module}/ssh/${var.ssh_key_name}.pem")
     })
-    
+
     interpreter = local.host_os == "windows" ? ["powershell.exe", "-command"] : ["bash", "-c"]
   }
 
@@ -140,41 +231,80 @@ resource "azurerm_linux_virtual_machine" "secOps-linux-vm-01" {
 
 
 locals {
-  os = data.external.os.result.os
+  os      = data.external.os.result.os
   host_os = local.os == "windows" ? "windows" : "linux"
+  hostname = "secops-vm-tf-${random_string.rstring.result}"
 }
 
 
-data "azurerm_public_ip" "secOps-ip-data" {
-  name                = azurerm_public_ip.secOps-ip.name
-  resource_group_name = azurerm_resource_group.secOps.name
+data "azurerm_public_ip" "secops_ip-data" {
+  name                = azurerm_public_ip.secops_ip.name
+  resource_group_name = azurerm_resource_group.secops.name
 }
 
 data "http" "my-home-ip" {
-  url = "https://ipv4.icanhazip.com"
+  url = "http://ipv4.icanhazip.com"
 }
 
 data "external" "host_username" {
-  program = local.os == "windows" ? ["powershell.exe","-c","${path.module}/get_host_user.ps1"] : ["bash","${path.module}/get_host_user.sh"]
+  program = local.os == "windows" ? ["powershell.exe", "-c", "${path.module}/get_host_user.ps1"] : ["bash", "${path.module}/get_host_user.sh"]
 }
 
 data "external" "os" {
   working_dir = path.module
-  program = ["printf", "{\"os\": \"linux\"}"]
+  program     = ["printf", "{\"os\": \"linux\"}"]
 }
 
+resource "null_resource" "create_ssh_dir" {
+  depends_on = [azurerm_key_vault_secret.ssh_private_key]
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.module}/ssh"
+    interpreter = ["bash", "-c"]
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+# We use the private key to connect to the Azure VM
+resource "local_file" "vm-ssh-private-key" {
+  depends_on = [null_resource.create_ssh_dir] 
+  content    = azurerm_key_vault_secret.ssh_private_key.value
+  filename   = "${path.module}/ssh/${var.ssh_key_name}.pem"
+}
+
+resource "null_resource" "set-perms-ssh_key" {
+  depends_on = [
+    local_file.vm-ssh-private-key,
+    null_resource.create_ssh_dir
+  ]
+  provisioner "local-exec" {
+    command = local.host_os == "linux" ? "chmod 400 ${path.module}/ssh/${var.ssh_key_name}.pem" : "icacls.exe ${path.module}\\ssh\\${var.ssh_key_name}.pem /inheritance:r"
+    interpreter = local.host_os == "linux" ? ["bash", "-c"] : ["powershell.exe", "-command"]
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+
+
 output "host_username" {
-  value = "${data.external.host_username.result.username}"
+  value = data.external.host_username.result.username
 }
 
 output "local_host_os" {
-  value = "${local.host_os}"
+  value = local.host_os
 }
 
 output "vm_username_bash_script" {
-  value = "${var.vm_username}"
+  value = var.vm_username
+}
+
+output "hostname_vm_tf" {
+  value = azurerm_linux_virtual_machine.secops-linux-vm.name
 }
 
 output "public_ip_address" {
-  value = "${azurerm_linux_virtual_machine.secOps-linux-vm-01.name}: ${data.azurerm_public_ip.secOps-ip-data.ip_address}"
+  value = azurerm_public_ip.secops_ip.*.ip_address
 }
