c7571dc0ae8b393706f7b469e090ad8b4bb722c7
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-10-11 11:51:08 -0400

    Update rhel88-vm-output.tf

--- terraform/redhat/8/rhel88-vm-output.tf
+++ terraform/redhat/8/rhel88-vm-output.tf
@@ -1,18 +1,25 @@
 ##########################
 ## Azure Linux - Output ##
 ##########################
-
-output "linux_vm_name" {
+output linux_vm_name {
   description = "Virtual Machine name"
   value       = azurerm_linux_virtual_machine.rhel88-vm.name
 }
 
-output "linux_vm_ip_address" {
+output linux_vm_ip_address {
   description = "Virtual Machine name IP Address"
   value       = azurerm_public_ip.rhel88-vm-ip.ip_address
 }
 
-output "linux_vm_admin_username" {
+output linux_vm_admin_username {
   description = "Username password for the Virtual Machine"
   value       = var.linux_username
+}
+
+output host_username {
+  value = data.external.host_username.result.username
+}
+
+output local_host_os {
+  value = local.host_os
 }
