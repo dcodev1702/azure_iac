390428ad58e2c98c660f52f8ae04e043a7f020a7
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-10-13 15:06:21 +0000

    Update and rename variables.tf to kv-variables.tf

---
+++ terraform/key_vault/kv-variables.tf
@@ -0,0 +1,36 @@
+variable azure_subscription_id {
+  type        = string
+  description = "Azure Subscription ID"
+}
+variable azure_tenant_id {
+  type        = string
+  description = "Azure Tenant ID"
+}
+variable tag_env {
+  type        = string
+  description = "Environment Tag"
+}
+variable key_vault_name {
+  type        = string
+  description = "Name of Key Vault"
+}
+variable key_vault_location {
+  type        = string
+  description = "Location of Key Vault"
+}
+variable "user_assigned_identity_name" {
+  type = string
+  description = "User Assigned Identity"
+}
+variable user_assigned_identity_guid {
+  type = string
+  description = "User Assigned Identity GUID"
+}
+variable "uai_resource_group_name" {
+  type = string
+  description = "User Assigned Identity Resource Group"
+}
+variable cloud_environment {
+  type = string
+  description = "public"
+}
