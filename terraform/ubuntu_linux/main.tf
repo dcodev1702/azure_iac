efe095524bbc90d7926b56a8aee0a5fad099668c
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-10-13 18:31:32 +0000

    Update main.tf

--- terraform/ubuntu_linux/main.tf
+++ terraform/ubuntu_linux/main.tf
@@ -256,6 +256,9 @@ resource azurerm_linux_virtual_machine secops-linux-vm {
     interpreter = local.host_os == "windows" ? ["powershell.exe", "-command"] : ["bash", "-c"]
   }
 
+  # Move's 50-default.conf in /etc/rsyslog.d to the system's location
+  # This conf file is set to forward it's auth & authpriv syslog to 10.120.1.4 (Syslog Collector Private IP)
+  # TCP on port 20514 bec that's the port pre-defined on RHEL 8.
   provisioner file {
     source      = "${path.module}/etc/rsyslog.d/50-default.conf"
     destination = "/home/${var.vm_username}/50-default.conf"
