bfa5509963694622cfa43ff2a8fc07c75cddcdcd
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-10-13 16:07:37 +0000

    Update output.tf

--- terraform/ubuntu_linux/output.tf
+++ terraform/ubuntu_linux/output.tf
@@ -1,19 +1,15 @@
 output host_username {
   value = data.external.host_username.result.username
 }
-
 output local_host_os {
   value = local.host_os
 }
-
 output vm_username_bash_script {
   value = var.vm_username
 }
-
 output hostname_vm_tf {
   value = azurerm_linux_virtual_machine.secops-linux-vm.name
 }
-
 output public_ip_address {
   value = data.azurerm_public_ip.secops.ip_address
 }
