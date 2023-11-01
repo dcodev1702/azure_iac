c1019384eb680dcdbbee5f92217e04381c59fb42
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-11-01 19:34:31 -0400

    Update kv-main.tf

--- terraform/key_vault/kv-main.tf
+++ terraform/key_vault/kv-main.tf
@@ -98,12 +98,3 @@ resource azurerm_key_vault_access_policy user_msi {
     "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
   ]
 }
-
-# Output name of Key Vault and add to variable.tf and terraform.tfvars
-# for Ubuntu and RHEL VM's so you can create and store SSH Keys in this Key Vault.
-output key_vault_name {
-  value = azurerm_key_vault.main.name
-}
-output key_vault_id {
-  value = azurerm_key_vault.main.id
-}
