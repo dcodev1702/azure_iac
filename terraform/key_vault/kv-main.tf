258cadc169e61269fc3f708a9683bbab779cb9f6
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-11-09 16:37:10 -0500

    Update kv-main.tf

--- terraform/key_vault/kv-main.tf
+++ terraform/key_vault/kv-main.tf
@@ -1,10 +1,10 @@
 # Define Terraform provider
 terraform {
-  required_version = "~> 1.6.2"
+  required_version = "~> 1.6.3"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.78.0"
+      version = "~> 3.79.0"
     }
     random = {
       source  = "hashicorp/random"
