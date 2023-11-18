e7e4a2d11604dabc0fdb58268c5adef644c2962c
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-11-18 12:16:47 -0500

    Update main.tf

--- terraform/ubuntu_linux/main.tf
+++ terraform/ubuntu_linux/main.tf
@@ -1,10 +1,10 @@
 # Configure the Azure provider
 terraform {
-  required_version = "~> 1.6.3"
+  required_version = "~> 1.6.4"
   required_providers {
     azurerm  = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.79.0"
+      version = "~> 3.81.0"
     }
     tls = {
       source  = "hashicorp/tls"
