f6378e7f00ee2a10ac454767d4101e91e6d35b1a
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-11-19 00:38:39 -0500

    Create dcr-variables.tf

---
+++ terraform/az_tf_syslog_data_collection_rule/dcr-variables.tf
@@ -0,0 +1,48 @@
+variable azure_subscription_id {
+  type        = string
+  description = "Azure Subscription ID"
+}
+variable azure_tenant_id {
+  type        = string
+  description = "Azure Tenant ID"
+}
+variable tag_env {
+  type        = string
+  description = "Environment Tag"
+}
+variable dcr_syslog_name {
+  type        = string
+  description = "Name of Data Collection Rule"
+}
+variable dcr_syslog_location {
+  type        = string
+  description = "Location of Data Collection Rule"
+}
+variable log_analytics_workspace_name {
+  type        = string
+  description = "Name of Log-A Workspace"
+}
+variable log_analytics_workspace_rg {
+  type        = string
+  description = "Log-A Resource Group"
+}
+variable user_assigned_identity_name {
+  type = string
+  description = "User Assigned Identity"
+}
+variable user_assigned_identity_guid {
+  type = string
+  description = "User Assigned Identity GUID"
+}
+variable uai_resource_group_name {
+  type = string
+  description = "User Assigned Identity Resource Group"
+}
+variable cloud_environment {
+  type = string
+  description = "Cloud Environment"
+}
+variable user_assigned_identity_endpoint {
+  type = string
+  description = "MSI Endpoint"
+}
