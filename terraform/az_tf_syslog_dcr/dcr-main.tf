36a97d3cffbc057fa0ebb2efd89180f3af46de02
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-12-05 18:26:48 -0500

    Update dcr-main.tf

--- terraform/az_tf_syslog_dcr/dcr-main.tf
+++ terraform/az_tf_syslog_dcr/dcr-main.tf
@@ -1,10 +1,10 @@
 # Define Terraform provider
 terraform {
-  required_version = "~> 1.6.4"
+  required_version = "~> 1.6.5"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.81.0"
+      version = "~> 3.83.0"
     }
     random = {
       source  = "hashicorp/random"
